a583b38d44dc95d53c8b0dc3f337994af0aeda33
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-05-30 14:04:06 -0500

    actualizaciones

--- js/main.js
+++ js/main.js
@@ -6,26 +6,26 @@ let idioma2 = "otro"
 
 let avanzar = true
 while(avanzar){
-    let edad = parseInt(prompt("Bienvenido!!! Ingrese su rango etario: 1 para menor o igual a 6, 2 si tenes entre 7 y 18 inclusive, 3 si sos mayor de 18, otro numero para salir"))
+    let edad = parseInt(prompt("Bienvenido!!! Ingrese su edad"))
     let idioma = prompt("Ingresa 'espanol' si te gusta la musica en espanol u 'otro' si preferis musica en otro idioma")
     switch(info) {
         case 1:
             let idioma = "espanol" || idioma = "otro"
-            if (edad <=6 && idioma == espanol) {
+            if (edad <= 6 && idioma == espanol) {
                 console.log("Tengo 6 o menos y quiero musica en espanol")
             } else {
                 console.log("Tengo 6 o menos y quiero musica en otro idioma")
             } 
             break
         case 2:
-            if (edad >=7 && edad<=18) && (idioma == espanol){
+            if (edad >= 7 && edad<= 18) && (idioma == espanol){
                 console.log("Tengo entre 7 y 18 y quiero musica en espanol")
             } else {
                 console.log("Tengo entre 7 y 18 y quiero musica en otro idioma")
             } 
             break
         case 3:
-            if (edad >18 && idioma == espanol) {
+            if (edad > 18 && idioma == espanol) {
                 console.log("Tengo mas de 18 y quiero musica en espanol")
             } else {
                 console.log("Tengo mas de 18 y quiero musica en otro idioma")
@@ -38,10 +38,12 @@ while(avanzar){
 
         let confirmacion = prompt("Listo para continuar? si/no")
         if (confirmacion == "no"){
-            avanzar = true
+            avanzar = false
             console.log("Gracias por ingresar! Que tenga un lindo dia")
         }
     }
+
+    avanzar()
 
 
 
