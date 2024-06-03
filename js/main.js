4bf0a1d627b349f28f38a9924c1e7d73184c7acc
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-06-02 20:01:51 -0500

    formato

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let edad1 = 6
-let edad2 = 18
-let edad3 = 25
-let idioma1 = "espanol"
-let idioma2 = "otro"
-let info = 'edad' + 'idioma'
+let idioma = "espanol"
+const cancion1 = "La Primavera Llego de El Reino Infantil"
+const cancion2 = "Old Mc Donald had a Farm de Super Simple Songs"
+const cancion3 = "Bzrp Music Sessions, Vol 53 con Shakira"
+const cancion4 = "Sorry de Justin Bieber"
+const cancion5 = "Crimen de Gustavo Cerati"
+const cancion6 = "Karma Chameleon de Boy George"
 
 let avanzar = true
 while(avanzar){
@@ -11,31 +12,31 @@ while(avanzar){
     let idioma = prompt("Ingresa 'espanol' si te gusta la musica en espanol u 'otro' si preferis musica en otro idioma")
     switch(idioma) {
         case 1:
-            let idioma = "espanol"  
+            let idioma = "espanol"
             if (edad <= 6 && idioma == espanol) {
                 console.log("Tengo 6 o menos y quiero musica en espanol")
-                alert = "Tu cancion es La Primavera Llego de El Reino Infantil"
+                alert("Tu cancion es: "+cancion1)
             } else {
                 console.log("Tengo 6 o menos y quiero musica en otro idioma")
-                alert = "Tu cancion es Old Mc Donald had a Farm de Super Simple Songs"
+                alert("Tu cancion es: "+cancion2)
             } 
             break
         case 2:
             if ((edad >= 7 && edad<= 18) && (idioma == espanol)){
                 console.log("Tengo entre 7 y 18 y quiero musica en espanol")
-                alert = "Tu cancion es Bzrp Music Sessions, Vol 53 con Shakira"
+                alert("Tu cancion es: "+cancion3)
             } else {
                 console.log("Tengo entre 7 y 18 y quiero musica en otro idioma")
-                alert = "Tu cancion es Sorry de Justin Bieber"
+                alert("Tu cancion es: "+cancion4)
             } 
             break
         case 3:
             if (edad > 18 && idioma == espanol) {
                 console.log("Tengo mas de 18 y quiero musica en espanol")
-                alert = "Tu cancion es Crimen de Gustavo Cerati"
+                alert("Tu cancion es: "+cancion5)
             } else {
                 console.log("Tengo mas de 18 y quiero musica en otro idioma")
-                alert = "Tu cancion es Karma Chameleon de Boy George"
+                alert("Tu cancion es: "+cancion6)
             } 
             break
         default:
@@ -47,9 +48,12 @@ while(avanzar){
         if (confirmacion == "no"){
             avanzar = false
             console.log("Gracias por ingresar! Que tenga un lindo dia")
+            alert("Muchas gracias por su visita!")
         }
     }
 
+    const canciones = [cancion1, cancion2, cancion3, cancion4, cancion5, cancion6]
+    canciones.push("La Vaca Lola de La Granja de Zenon")
 
 
 
